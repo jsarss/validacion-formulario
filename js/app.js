bf00eb94daea83390bcd17bf101088a27156b4f3
jsar <albertorincon2121@gmail.com>
2023-07-03 23:40:15 -0500

    proceso de validacion

---
+++ js/app.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    //Seleccionar elementos del html
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+
+
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+
+    function validar(e){
+       if(e.target.value.trim() === ''){
+           mostarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+         return;
+       }
+
+        if(e.target.id === "email" && !validarEmail(e.target.value)){
+            mostarAlerta(`El campo Email no es valido`, e.target.parentElement);
+            return;
+        }
+       limpiarAlerta(e.target.parentElement);
+    }
+
+
+    function mostarAlerta(mensaje, referencia){
+        limpiarAlerta(referencia);
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        referencia.appendChild(error);
+
+
+       
+    }
+
+    function limpiarAlerta(referencia){
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove()
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+
+        console.log(resultado);
+    }
+
+
+});
